7154b06e1ab13debd06da97be41e1824cf321c36
Dave Mac <ihategates@hotmail.com>
2018-02-16 11:52:27 +1100

    Adding extra methods to the REST service

--- EasyRestApi/testapi.py
+++ EasyRestApi/testapi.py
@@ -1,7 +1,11 @@
 #!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, make_response
 
 app = Flask(__name__)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 @app.route('/')
 def index():
@@ -25,6 +29,13 @@ tasks = [
 @app.route('/testapi/tasks', methods=['GET'])
 def get_tasks():
     return jsonify({ 'tasks': tasks })
+
+@app.route('/testapi/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
 
 if __name__ == '__main__':
     app.run(debug=True)
