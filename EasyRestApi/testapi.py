61330e01e0ea66bee684f2e06473dafe5182dcde
Dave Mac <ihategates@hotmail.com>
2018-02-16 11:31:33 +1100

    Hello world service in flask! w00t

--- EasyRestApi/testapi.py
+++ EasyRestApi/testapi.py
@@ -1,7 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Feb 16 11:15:41 2018
+#!flask/bin/python
+from flask import Flask
 
-@author: Dave
-"""
+app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
